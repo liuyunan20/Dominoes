2c6a48a7615ef0c97d864a71967ee721273b3100
liuyunan <liuyunan.20@gmail.com>
2021-12-04 01:48:59 -0800

    need to remove [] outside snake when printing

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -34,21 +34,77 @@ while reshuffle:
             break
         else:
             reshuffle = True
-# print(f"Stock pieces: {stock}")
-# print(f"Computer pieces: {computer}")
-print("=" * 70)
-print(f"Stock size: {len(stock)}")
-print(f"Computer pieces: {len(computer)}")
-print()
-print(snake[0])
-print()
-print("Your pieces:")
-n = 1
-for piece in player:
-    print(f"{n}:{piece}")
-    n += 1
-print()
-if status == "player":
-    print("Status: It's your turn to make a move. Enter your command.")
-elif status == "computer":
-    print("Status: Computer is about to make a move. Press Enter to continue...")
+
+game_result = ""
+game_continue = True
+
+
+# define a function for checking game end
+def check_end():
+    global game_result
+    global game_continue
+    if len(player) == 0:
+        game_result = "You won!"
+        game_continue = False
+    elif len(computer) == 0:
+        game_result = "The computer won!"
+        game_continue = False
+    elif snake[0][0] == snake[-1][-1] and snake.count(snake[0][0]) == 8:
+        game_result = "It's a draw!"
+        game_continue = False
+    else:
+        game_continue = True
+
+
+def make_move(part, part_move):
+    if int(part_move) > 0:
+        snake.append(part.pop(int(part_move) - 1))
+    elif int(part_move) < 0:
+        snake.insert(0, part.pop(-int(part_move) - 1))
+    elif int(player_move) == 0:
+        part.append(stock.pop(random.randint(0, len(stock))))
+
+
+while game_continue:
+    print("=" * 70)
+    print(f"Stock size: {len(stock)}")
+    print(f"Computer pieces: {len(computer)}")
+    print()
+    if len(snake) <= 6:
+        print(snake)
+    else:
+        print(f"{snake[0],snake[1],snake[2]}...{snake[-3],snake[-2],snake[-1]}")
+    print()
+    print("Your pieces:")
+    n = 1
+    for piece in player:
+        print(f"{n}:{piece}")
+        n += 1
+    print()
+    check_end()
+    if game_continue:
+        if status == "player":
+            print("Status: It's your turn to make a move. Enter your command.")
+            input_invalid = True
+            while input_invalid:
+                player_move = input()
+                try:
+                    int(player_move)
+                except:
+                    print("Invalid input. Please try again.")
+                else:
+                    if -len(player) <= int(player_move) <= len(player):
+                        input_invalid = False
+                        break
+                    else:
+                        print("Invalid input. Please try again.")
+            make_move(player, player_move)
+            status = "computer"
+        elif status == "computer":
+            print("Status: Computer is about to make a move. Press Enter to continue...")
+            input()
+            computer_move = random.randint(-len(computer), len(computer) + 1)
+            make_move(computer, computer_move)
+            status = "player"
+    else:
+        print(f"Status: The game is over. {game_result}")
---
+++ Topics/Math functions/Logistic function/main.py
@@ -0,0 +1,5 @@
+import math
+x = int(input())
+sig = math.exp(x) / (math.exp(x) + 1)
+print(round(sig, 2))
+
