5e7f28f4c303df910cff8c96a03c2449ae57d5be
liuyunan <liuyunan.20@gmail.com>
2021-12-04 20:32:57 -0800

    move follow the game rules

--- Dominoes/task/dominoes/dominoes.py
+++ Dominoes/task/dominoes/dominoes.py
@@ -37,6 +37,7 @@ while reshuffle:
 
 game_result = ""
 game_continue = True
+stock_empty = False
 
 
 # define a function for checking game end
@@ -52,17 +53,46 @@ def check_end():
     elif snake[0][0] == snake[-1][-1] and snake.count(snake[0][0]) == 8:
         game_result = "It's a draw!"
         game_continue = False
+    elif stock_empty:
+        game_result = "It's a draw!"
+        game_continue = False
     else:
         game_continue = True
 
 
 def make_move(part, part_move):
+    global move_illegal
+    global stock_empty
     if int(part_move) > 0:
-        snake.append(part.pop(int(part_move) - 1))
+        if snake[-1][-1] == part[int(part_move) - 1][0]:
+            snake.append(part.pop(int(part_move) - 1))
+            move_illegal = False
+        elif snake[-1][-1] == part[int(part_move) - 1][1]:
+            x = part[int(part_move) - 1][1]
+            part[int(part_move) - 1][1] = part[int(part_move) - 1][0]
+            part[int(part_move) - 1][0] = x
+            snake.append(part.pop(int(part_move) - 1))
+            move_illegal = False
+        else:
+            move_illegal = True
     elif int(part_move) < 0:
-        snake.insert(0, part.pop(-int(part_move) - 1))
-    elif int(player_move) == 0:
-        part.append(stock.pop(random.randint(0, len(stock) - 1)))
+        if snake[0][0] == part[-int(part_move) - 1][1]:
+            snake.insert(0, part.pop(-int(part_move) - 1))
+            move_illegal = False
+        elif snake[0][0] == part[-int(part_move) - 1][0]:
+            x = part[-int(part_move) - 1][1]
+            part[-int(part_move) - 1][1] = part[-int(part_move) - 1][0]
+            part[-int(part_move) - 1][0] = x
+            snake.insert(0, part.pop(-int(part_move) - 1))
+            move_illegal = False
+        else:
+            move_illegal = True
+    elif int(part_move) == 0:
+        if len(stock) == 0:
+            stock_empty = True
+        else:
+            part.append(stock.pop(random.randint(0, len(stock) - 1)))
+        move_illegal = False
 
 
 while game_continue:
@@ -86,24 +116,32 @@ while game_continue:
         if status == "player":
             print("Status: It's your turn to make a move. Enter your command.")
             input_invalid = True
-            while input_invalid:
-                player_move = input()
-                try:
-                    int(player_move)
-                except:
-                    print("Invalid input. Please try again.")
-                else:
-                    if -len(player) <= int(player_move) <= len(player):
-                        input_invalid = False
-                        break
-                    else:
+            move_illegal = True
+            while move_illegal:
+                input_invalid = True
+                while input_invalid:
+                    player_move = input()
+                    try:
+                        int(player_move)
+                    except:
                         print("Invalid input. Please try again.")
-            make_move(player, player_move)
+                    else:
+                        if -len(player) <= int(player_move) <= len(player):
+                            input_invalid = False
+                            break
+                        else:
+                            print("Invalid input. Please try again.")
+
+                make_move(player, player_move)
+                if move_illegal:
+                    print("Illegal move. Please try again.")
             status = "computer"
         elif status == "computer":
             input("Status: Computer is about to make a move. Press Enter to continue...")
-            computer_move = random.randint(-len(computer), len(computer))
-            make_move(computer, computer_move)
+            move_illegal = True
+            while move_illegal:
+                computer_move = random.randint(-len(computer), len(computer))
+                make_move(computer, computer_move)
             status = "player"
     else:
         print(f"Status: The game is over. {game_result}")
